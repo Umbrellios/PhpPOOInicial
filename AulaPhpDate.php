e43616127e21cb1b0c8e2e7468835eeb313b1381
Braulio J. S. Santos <braulio.j.s.santos@gmail.com>
2021-10-14 21:52:29 -0300

    Iniciado aula manipulando datas e horas

---
+++ AulaPhpDate.php
@@ -0,0 +1,5 @@
+<?php
+
+$data = new DateTime();
+print_r ($data);
+print_r ($data->format('d-m-Y')) . PHP_EOL;
--- ContaBancaria.php
+++ ContaBancaria.php
@@ -1,16 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 class ContaBancaria
 {
+    private string $banco;
+    private string $nomeTitular;
+    private string $numeroAgencia;
+    private string $numeroConta;
+    private float $saldo;
 
-    private $banco;
-    private $nomeTitular;
-    private $numeroAgencia;
-    private $numeroConta;
-    private $saldo;
-
-
-    public function __construct($banco, $nomeTitular, $numeroAgencia, $numeroConta, $saldo)
+    public function __construct(
+        string $banco,
+        string $nomeTitular,
+        string $numeroAgencia,
+        string $numeroConta,
+        float $saldo)
     {
         $this->banco = $banco;
         $this->nomeTitular = $nomeTitular;
@@ -20,18 +25,18 @@ class ContaBancaria
 
     }
 
-    public function obterSaldo()
+    public function obterSaldo(): string
     {
         return 'Seu saldo atual é: r$ '. $this->saldo;
     }
 
-    public function depositar($valor)
+    public function depositar(float $valor): string
     {
         $this->saldo += $valor;
         return 'Deposito no valor de R$: '. $valor. ' Realizado';
     }
 
-    public function saque($valor)
+    public function saque(float $valor): string
     {
         $this->saldo -= $valor;
         return 'Saque no valor de R$: '. $valor. ' Realizado';
@@ -39,7 +44,7 @@ class ContaBancaria
 
 }
 
-$conta1 = new contaBancaria
+$conta = new contaBancaria
 (
     'Banco do Brasil',
     'Braulio Santos',
@@ -48,28 +53,19 @@ $conta1 = new contaBancaria
     1000.00
 );
 
-$conta2 = new contaBancaria
-(
-    'Banco do Brasil',
-    'Braulio Santos',
-    '874-1',
-    '30643-7',
-    1000.00
-);
+//var_dump ($conta);
 
-var_dump ($conta1);
-var_dump ($conta2);
-//echo ($conta->obterSaldo());
-//
-//echo PHP_EOL;
-//echo $conta->depositar(200);
-//
-//echo PHP_EOL;
-//print_r ($conta->obterSaldo());
-//
-//echo PHP_EOL;
-//echo $conta->saque(100);
-//
-//echo PHP_EOL;
-//print_r ($conta->obterSaldo());
+echo ($conta->obterSaldo());
+
+echo PHP_EOL;
+echo $conta->depositar(200);
+
+echo PHP_EOL;
+print_r ($conta->obterSaldo());
+
+echo PHP_EOL;
+echo $conta->saque(100);
+
+echo PHP_EOL;
+print_r ($conta->obterSaldo());
 
