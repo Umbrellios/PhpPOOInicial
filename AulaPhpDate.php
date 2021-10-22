eed639b33b80ffba5bd337b074bc0815454071d8
Braulio J. S. Santos <braulio.j.s.santos@gmail.com>
2021-10-21 21:42:59 -0300

    Efetuando testes com valore Date

--- AulaPhpDate.php
+++ AulaPhpDate.php
@@ -1,5 +1,13 @@
 <?php
 
 $data = new DateTime();
-print_r ($data);
-print_r ($data->format('d-m-Y')) . PHP_EOL;
+
+$intervalo = new DateInterval('P5Y10M5DT10H50M10S');
+//$data->add($intervalo);
+$data->sub($intervalo);  
+var_dump($data);
+
+//print_r ($data);
+//print_r ($data->format('d-m-Y H:i:s')) . PHP_EOL;
+
+//
